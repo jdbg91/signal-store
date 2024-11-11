e1a2717e9f79ddb5c84219a9db723ad558ac7552
Jose Borges <jdbg_91@hotmail.com>
2024-11-11 19:09:15 +0000

    Added toggle component

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,6 +1,6 @@
 import {inject, Injectable} from "@angular/core";
 import {finalize, map, Observable, scan, startWith} from "rxjs";
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root',
